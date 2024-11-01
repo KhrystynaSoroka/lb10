185710994789518908dc7d435369e32ca556ad4a
KhrystynaSoroka <sorokakhristina05@gmail.com>
2024-11-01 16:31:11 +0200

    четверте завдання

--- ex3.py
+++ ex3.py
@@ -1,5 +1,6 @@
 flights = []
 n = int(input("Введіть кількість авіарейсів для додавання: "))
+
 for i in range(n):
     print(f"\nВведіть інформацію про рейс {i + 1}:")
     flight = {}
@@ -8,7 +9,13 @@ for i in range(n):
     flight['destination'] = input("  Введіть місце призначення: ")
     flight['total_seats'] = int(input("  Введіть кількість місць для пасажирів (шт., додатне ціле число): "))
     flight['tickets_sold'] = int(input("  Введіть кількість проданих квитків (шт., додатне ціле число): "))
+    baggage = {}
+    baggage['type'] = input("  Введіть тип багажу: ")
+    baggage['total_weight'] = float(input("  Введіть загальну вагу багажу (в кг., додатне дійсне число): "))
+    baggage['estimated_value'] = int(input("  Введіть оціночну вартість багажу (в грн., додатне ціле число): "))
+    flight['baggage'] = baggage
     flights.append(flight)
+
 print("\nІнформація про всі авіарейси:")
 for i, flight in enumerate(flights, start=1):
     print(f"\nРейс {i}:")
@@ -17,3 +24,7 @@ for i, flight in enumerate(flights, start=1):
     print(f"  Місце призначення: {flight['destination']}")
     print(f"  Кількість місць для пасажирів: {flight['total_seats']}")
     print(f"  Кількість проданих квитків: {flight['tickets_sold']}")
+    print("  Інформація про багаж:")
+    print(f"    Тип багажу: {flight['baggage']['type']}")
+    print(f"    Загальна вага багажу: {flight['baggage']['total_weight']} кг")
+    print(f"    Оціночна вартість багажу: {flight['baggage']['estimated_value']} грн")
